f6b883d1e2ad9184d87f20f99b80c7baa567c019
oplataprosvetis-stack <oplata.prosvetis@gmail.com>
2026-05-25 12:13:31 +0500

    Add files via upload

---
+++ laravel/database/factories/CategoryFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->words(2, true);
+
+        return [
+            'name'      => $name,
+            'slug'      => Str::slug($name),
+            'parent_id' => null,
+        ];
+    }
+}
